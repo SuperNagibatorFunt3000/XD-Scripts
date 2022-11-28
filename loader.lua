c54d1990395a38ce872cad80710a3cf0a0e45bfb
SuperNagibatorFunt3000 <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2022-11-28 19:11:23 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,7 @@
 if game.PlaceId == 2809202155 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/XD-Scripts/main/YBA.lua"))()
 end
+
+if game.PlaceId == 6298464951 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/XD-Scripts/main/RIU.lua"))()
+end
